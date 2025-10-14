619de674dbd1d5e625aa9795e4a34762e2278a20
Daniel Koska <koda86@users.noreply.github.com>
2025-10-14 14:50:00 +0200

    replaced example section in yaml header

--- R/band.R
+++ R/band.R
@@ -24,42 +24,9 @@
 #'   (a list with settings such as type, alpha, iid, B, n, T).
 #'
 #' @examples
-#' ## i.i.d. example (small, fast) with shaded band
-#' set.seed(1)
-#' n_time <- 60; n <- 8
-#' Y <- matrix(rnorm(n_time * n, sd = 0.25), nrow = n_time, ncol = n) +
-#'      outer(seq_len(n_time), rep(1, n), function(i, j) 0.5 * sin(2*pi*i/n_time))
-#' fit <- band(Y, type = "prediction", alpha = 0.1, iid = TRUE, B = 25L, k.coef = 12L)
-#' x <- seq_len(fit$meta$T)
-#' plot(x, fit$mean, type = "n",
-#'      ylim = range(c(fit$lower, fit$upper)), xlab = "Index", ylab = "Value")
-#' polygon(c(x, rev(x)), c(fit$lower, rev(fit$upper)),
-#'         col = grDevices::adjustcolor("steelblue", alpha.f = 0.3), border = NA)
-#' lines(x, fit$mean, lwd = 2)
-#'
-#' ## clustered example (also kept small and fast)
-#' set.seed(2)
-#' n_time  <- 80; m <- c(4, 4)                 # two clusters, few curves
-#' t  <- seq(0, 1, length.out = n_time)
-#' mu <- list(function(x) 0.7 * sin(2*pi*x),
-#'            function(x) 0.6 * cos(2*pi*x))
-#' Bm <- cbind(sin(2*pi*t), cos(2*pi*t))
-#' gen_curve <- function(k) {
-#'   sc <- rnorm(ncol(Bm), sd = c(0.2, 0.15))
-#'   mu[[k]](t) + as.vector(Bm %*% sc) + rnorm(n_time, sd = 0.12)
-#' }
-#' Ylist <- lapply(seq_along(m), function(k) sapply(seq_len(m[k]), function(i) gen_curve(k)))
-#' Yh    <- do.call(cbind, Ylist)
-#' id    <- rep(seq_along(m), times = m)
-#' fitH  <- band(Yh, type = "prediction", alpha = 0.1,
-#'               iid = FALSE, id = id, B = 25L, k.coef = 12L)
-#' xh <- seq_len(fitH$meta$T)
-#' plot(xh, fitH$mean, type = "n",
-#'      ylim = range(c(Yh, fitH$lower, fitH$upper), finite = TRUE),
-#'      xlab = "Index", ylab = "Value")
-#' polygon(c(xh, rev(xh)), c(fitH$lower, rev(fitH$upper)),
-#'         col = grDevices::adjustcolor("steelblue", alpha.f = 0.30), border = NA)
-#' lines(xh, fitH$mean,  lwd = 2)
+#' @example inst/examples/iid_example.R
+#' @examples
+#' @example inst/examples/clustered_example.R
 #'
 #' @references
 #' Koska, D., Oriwol, D., & Maiwald, C. (2023).
